9794f50703727db896ca440ff654cc21a7a59717
DevIA05 <111973830+DevIA05@users.noreply.github.com>
2023-06-21 08:55:51 +0400

    update et correction

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,9 +1,6 @@
 import type { Component } from 'solid-js';
 
-import logo from './logo.svg';
-import styles from './App.module.css';
 import Table from './components/Table';
-import Modal from './components/Modal';
 
 const App: Component = () => {
   return (
